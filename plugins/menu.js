59db0d13ae2edaf638a920e45edd84c83f808a94
PlayBoys <101332428+PlayyBoys@users.noreply.github.com>
2022-03-12 21:31:55 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -36,7 +36,6 @@ let tags = {
 
 const defaultMenu = {
   before: `
-Hi! ${name}, ${ucapan()}
 ╭─❏ *Info*
 ├▸ *Name:* %name
 ├▸ *Premium:* 404
@@ -163,7 +162,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send3ButtonImg(m.chat, await (await fetch(image)).buffer(), `ㅤㅤ`
+    await conn.send3ButtonImg(m.chat, await (await fetch(image)).buffer(), `*Hi!, ${name} ${ucapan()}*\n*I'am ᴍɪᴋᴜ-ʙᴏᴛ*`
 , text.trim(), 'Owner Bot', `,owner`, 'Rules', '.rules', 'Donasi', '.donasi', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
