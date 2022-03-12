082c461aabd6e63962705135d33d1cbab544f58d
PlayBoys <101332428+PlayyBoys@users.noreply.github.com>
2022-03-12 16:18:03 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -587,7 +587,7 @@ Kamu Belum Terdaftar Di Database Bot
 Silahkan Daftar Dengan Klik Button Dibawah 
 `
 }[type]
-  if (msgg) return conn.sendButton(m.chat, msgg, global.wm, 'REGISTRASI', `.daftar ${namae}.18`, m, { contextInfo: { mentionedJid: [syappa] }})
+  if (msgg) return conn.sendButton(m.chat, msgg, wm, 'REGISTRASI', `.daftar ${namae}.18`, m, { contextInfo: { mentionedJid: [syappa] }})
 }
 
 let fs = require('fs')
