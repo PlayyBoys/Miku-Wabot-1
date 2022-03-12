9c777b29ddea998ab9b5b5c6e0df0b82f211e39b
PlayBoys <101332428+PlayyBoys@users.noreply.github.com>
2022-03-12 16:09:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -74,6 +74,7 @@ global.eror = '_*Server Error*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.image = 'https://telegra.ph/file/301eb8d95f70026e5aa90.jpg'
+global.wm = 'Daftar Dulu Onii Chan!'
 
 // tingkat kesulitan, semakin tinggi semakin susah
 global.multiplier = 250 // The higher, The harder levelup
